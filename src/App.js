e4681a64ed0e0c41ba7c870d053fa980ea5a004a
Tom Monks <monksy@gmail.com>
2022-03-09 12:29:39 -0500

    Add controls, plane, stars, & physics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import { Canvas } from "@react-three/fiber";
-import './styles.css';
+import { OrbitControls, Stars } from "@react-three/drei";
+import { useBox, usePlane, Physics } from "@react-three/cannon";
+import "./styles.css";
 
 function Box() {
+  const [ref] = useBox(() => ({ mass: 1 }));
   return (
-    <mesh>
+    <mesh ref={ref} position={[0, 2, 0]}>
       <boxBufferGeometry attach="geometry" />
       <meshLambertMaterial attach="material" color="hotpink" />
     </mesh>
-  )
+  );
 }
+
+function Plane() {
+  const [ref, api] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
+  return (
+    <mesh
+      onClick={() => {
+        api.velocity.set(0, 2, 0);
+      }}
+      position={[0, 0, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+    >
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  );
+}
+
 function App() {
-  return <Canvas><Box /></Canvas>
+  return (
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Physics>
+        <Box />
+        <Plane />
+      </Physics>
+    </Canvas>
+  );
 }
 
 export default App;
